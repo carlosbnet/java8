4e61e7ce7f2e409498230277fad44a0689933e99
carlosbnet <vandson.tec@gmail.com>
2020-02-18 02:33:50 -0300

    Criando a busca pela rede local

--- Praticas Java.io/Praticas IO variadas/src/br/com/TesteSocket.java
+++ Praticas Java.io/Praticas IO variadas/src/br/com/TesteSocket.java
@@ -5,18 +5,21 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Scanner;
 
-public class TesteSockets {
+
+public class TesteSocket {
     public static void main(String[] args) throws IOException {
 
-        ServerSocket servidor = new ServerSocket(3333);
+        ServerSocket servidor = new ServerSocket(3333);// Configura a porta 3333 para receber solicitações de outros endereços
 
 
         System.out.println("Iniciando Servidor...");
 
 
+
         Socket cliente = servidor.accept();
+
         System.out.println("Cliente "+cliente.getInetAddress().getHostAddress()+"  se conectou!");
-        servidor.
+
         Scanner input = new Scanner(cliente.getInputStream());
 
 
--- Praticas Java.io/Praticas IO variadas/src/br/com/TesteSocketClient.java
+++ Praticas Java.io/Praticas IO variadas/src/br/com/TesteSocketClient.java
@@ -1,28 +1,79 @@
 package br.com;
 
 import java.io.*;
-import java.net.Socket;
+import java.net.*;
+import java.util.Enumeration;
+import java.util.List;
 import java.util.Scanner;
 
 public class TesteSocketClient {
     public static void main(String[] args) throws IOException {
 
-        Socket cliente = new Socket("127.0.0.1",3333);
 
-        Scanner input = new Scanner(System.in);
-
+        String[] ip_fatiado = new String[0];
 
 
-        PrintStream dados = new PrintStream(cliente.getOutputStream());
-
-        System.out.println(cliente.getInetAddress().getAddress());
-        System.out.println(cliente.getInetAddress());
-        System.out.println(cliente.getInetAddress().getHostAddress());
-        System.out.println(cliente.getInetAddress().getHostName());
+        ServerSocket servidor = new ServerSocket(3333);// Configura a porta 3333 para receber solicitações de outros endereços
 
 
-      while (input.hasNextLine())
-        dados.println(input.nextLine());
+        Enumeration<NetworkInterface> buscEnd = NetworkInterface.getNetworkInterfaces();
 
+
+        while (buscEnd.hasMoreElements()) {
+
+            List endereco = buscEnd.nextElement().getInterfaceAddresses();
+            if (buscEnd.nextElement().isLoopback()) {
+
+                String endBruto = endereco.get(1).toString();
+                String[] end_limpo = endBruto.split("/");
+                ip_fatiado = end_limpo[1].split("\\.");
+                break;
+            }
+
+        }
+
+
+        for (int x = 100; x < 120; x++) {
+
+
+            Integer endLocal = Integer.parseInt(ip_fatiado[3]);
+
+            endLocal = x
+            ;
+
+            String novoValor = new String(endLocal.toString());
+
+            ip_fatiado[3] = novoValor;
+
+            String ip = String.join(".",ip_fatiado);
+
+         try {
+             Socket client = new Socket(ip, 3333);
+             client.setSoTimeout(10);
+             if (client.isConnected()){
+                 System.out.println("Um novo amigo foi encontrado!");
+                 client.close();
+             }
+         }catch (Exception e){
+             System.out.println(e.getMessage());
+             System.out.println(ip);
+         }
+
+
+
+
+
+
+            //  Scanner input = new Scanner(System.in);
+
+
+//        PrintStream dados = new PrintStream(cliente.getOutputStream());
+//
+//
+//
+//      while (input.hasNextLine())
+//        dados.println(input.nextLine());
+
+        }
     }
 }
